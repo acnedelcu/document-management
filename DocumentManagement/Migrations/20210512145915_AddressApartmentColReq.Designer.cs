f1c933c91b2f1c1937107ea4867dc7a6d1a5969a
Cosmin <andreicosminnedelcu@gmail.com>
2021-05-12 18:03:09 +0300

    Added the apartment property.

--- DocumentManagement/Migrations/20210512145915_AddressApartmentColReq.Designer.cs
+++ DocumentManagement/Migrations/20210512145915_AddressApartmentColReq.Designer.cs
@@ -4,14 +4,16 @@ using DocumentManagement.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DocumentManagement.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210512145915_AddressApartmentColReq")]
+    partial class AddressApartmentColReq
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- DocumentManagement/Models/Address.cs
+++ DocumentManagement/Models/Address.cs
@@ -13,13 +13,15 @@ namespace DocumentManagement.Models
         public int AddressId { get; set; }
 
         public string Street { get; set; }
-
+       
         public string Number { get; set; }
 
+        public string Apartment { get; set; }
+        
         public string City { get; set; }
-
+        
         public string County { get; set; }
-
+       
         public string Country { get; set; }
 
         public List<Faculty> Faculties { get; set; }
