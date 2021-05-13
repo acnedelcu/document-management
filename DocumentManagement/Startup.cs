ddb1da29ac4feddefae2f2a2729bdd47a6981770
Cosmin <andreicosminnedelcu@gmail.com>
2021-05-13 20:14:56 +0300

    Added intial view and controller.

--- DocumentManagement/Startup.cs
+++ DocumentManagement/Startup.cs
@@ -41,15 +41,14 @@ namespace DocumentManagement
             }
 
             app.UseHttpsRedirection();
-
+            app.UseStaticFiles();
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
