ad8ba83cf343ac0eb3c3fe5cc4c320b57bce02c9
Cosmin <andreicosminnedelcu@gmail.com>
2021-05-25 17:13:06 +0300

    Created interface and class for the StudyProgram.cs class.

---
+++ DocumentManagement/Models/IStudyProgramRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DocumentManagement.Models
+{
+    interface IStudyProgramRepository
+    {
+        public IEnumerable<StudyProgram> AllStudyPrograms { get; }
+    }
+}
---
+++ DocumentManagement/Models/StudyProgramRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DocumentManagement.Models
+{
+    public class StudyProgramRepository: IStudyProgramRepository
+    {
+        private readonly AppDbContext appDbContext;
+        public StudyProgramRepository(AppDbContext appDbContext)
+        {
+            this.appDbContext = appDbContext;
+        }
+        public IEnumerable<StudyProgram> AllStudyPrograms
+        {
+            get
+            {
+                return this.appDbContext.StudyPrograms;
+            }
+        }
+    }
+}
