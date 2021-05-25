7584d3885b0610b7111a7fefc315f78ab20a2d8d
Cosmin <andreicosminnedelcu@gmail.com>
2021-05-25 15:11:05 +0300

    Added controller and action for the file upload page.

---
+++ DocumentManagement/Controllers/UploadController.cs
@@ -0,0 +1,17 @@
+﻿using DocumentManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DocumentManagement.Controllers
+{
+    public class UploadController : Controller
+    {
+        public ViewResult Send()
+        {
+            return View();
+        }
+    }
+}
