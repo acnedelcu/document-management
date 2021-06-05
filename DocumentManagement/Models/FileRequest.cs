1bc34ccda371cce609e010e5410dddb9a0bf5b65
Cosmin <andreicosminnedelcu@gmail.com>
2021-06-05 14:52:41 +0300

    Created the FileRequest class.

---
+++ DocumentManagement/Models/FileRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DocumentManagement.Models
+{
+    public class FileRequest
+    {
+        public ApplicationUser ApplicationUser { get; set; }
+
+        public string DocumentType { get; set; }
+
+        public string Description { get; set; }
+
+    }
+}
