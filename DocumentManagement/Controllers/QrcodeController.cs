9633dbccd513d1de3966d7e792a5b2ac5b19576e
Cosmin <andreicosminnedelcu@gmail.com>
2021-06-07 17:41:30 +0300

    Added new controller and view for QR codes.

---
+++ DocumentManagement/Controllers/QrcodeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DocumentManagement.Controllers
+{
+    public class QrcodeController : Controller
+    {
+        public IActionResult Generate()
+        {
+            return View();
+        }
+    }
+}
