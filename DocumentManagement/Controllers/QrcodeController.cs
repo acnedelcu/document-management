653cc5ab26bc2bd097c2641d92924a16aa9a4757
Cosmin <andreicosminnedelcu@gmail.com>
2021-06-08 14:48:09 +0300

    Modified view and added controller action.

--- DocumentManagement/Controllers/QrcodeController.cs
+++ DocumentManagement/Controllers/QrcodeController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +9,27 @@ namespace DocumentManagement.Controllers
 {
     public class QrcodeController : Controller
     {
+        private static readonly string GeneratedQrFileName = "AccountQrCode.bmp";
+        private static readonly string DocumentsListUrl = "Document/List";
         public IActionResult Generate()
         {
             return View();
+        }
+
+        public IActionResult DownloadQr()
+        {
+            byte[] qrCode;
+            using(var memoryStream = new MemoryStream())
+            {
+                var generatedQrCode = Helper.GenerateQrCodeForUrl(DocumentsListUrl);
+                generatedQrCode.Save(memoryStream, System.Drawing.Imaging.ImageFormat.Bmp);
+                qrCode = memoryStream.ToArray();
+            }
+            var content = new System.IO.MemoryStream(qrCode);
+            var contentType = "APPLICATION/octet-stream";
+            var fileName = GeneratedQrFileName;
+
+            return File(content, contentType, fileName);
         }
     }
 }
