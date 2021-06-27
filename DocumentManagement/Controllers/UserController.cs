e1e330dd232e6c7c7939bb9d3a844859c8b00e22
Cosmin <andreicosminnedelcu@gmail.com>
2021-06-27 17:56:47 +0300

    Created new controller with action metods for the import of users.

---
+++ DocumentManagement/Controllers/UserController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+using DocumentManagement.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace DocumentManagement.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class UserController : Controller
+    {
+        private readonly IWebHostEnvironment webHostEnvironment;
+        private readonly UserManager<ApplicationUser> userManager;
+        private const string DefaultPassword = "P@rola123";
+        public UserController(IWebHostEnvironment webHostEnvironment, UserManager<ApplicationUser> userManager)
+        {
+            this.webHostEnvironment = webHostEnvironment;
+            this.userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult Import()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ImportUserAsync()
+        {
+            List<ApplicationUser> applicationUsers = new List<ApplicationUser>();
+
+            var file = Request.Form.Files["File"];
+            string filePath = @Path.Combine(webHostEnvironment.WebRootPath, "userImports");
+            string filePathWithFileName = Path.Combine(filePath, file.FileName);
+
+            if (!Directory.Exists(filePath))
+                Directory.CreateDirectory(filePath);
+
+            using (var fileStream = System.IO.File.Create(@filePathWithFileName))
+            {
+                file.CopyTo(fileStream);
+            }
+
+            //read user info from the excel file
+            applicationUsers = Helper.ReadExcelData(@filePathWithFileName);
+            if(applicationUsers.Count!=0)
+            {
+                foreach(var appUser in applicationUsers)
+                {
+                    await userManager.CreateAsync(appUser, DefaultPassword);
+                }
+            }
+
+            System.IO.File.Delete(@filePathWithFileName);
+            return View("Import");
+        }
+    }
+}
